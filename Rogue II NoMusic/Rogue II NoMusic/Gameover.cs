03209174cbabd7628891f485c11016a2fb6442e9
quinn <36234189+316968@users.noreply.github.com>
2018-05-29 13:40:33 -0400

    Create Gameover.cs

---
+++ Rogue II NoMusic/Rogue II NoMusic/Gameover.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Rogue_II_NoMusic
+{
+    class Gameover
+    {   public Rectangle gameoverScreen = new Rectangle();      
+        public Gameover(Canvas c , MainWindow w)
+        {
+            
+            gameoverScreen.Height = 800;
+            gameoverScreen.Width = 1200;
+            gameoverScreen.Fill = Brushes.Black;
+            gameoverScreen.Visibility = Visibility.Hidden;
+           
+        }
+    }
+}
